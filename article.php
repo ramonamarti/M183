1635914e95a6a1ea0c8488fc289a0c0c5bfb7c2a
ramonamarti <ramonamarti@gmx.net>
2020-03-28 11:46:54 +0100

    first hash

--- article.php
+++ article.php
@@ -12,9 +12,6 @@ if(!(isset($_SESSION['user'])&&$_SESSION['user']=='admin')){
     <script src="js-library.js"></script>
 </head>
 <body>
-<a href="files.php" class="link">Dateien</a>
-<a href="article.php" class="link">Artikel</a>
-<a href="login.php?logout=true" class="link logout">Logout</a>
 
 </body>
 </html>
--- main.php
+++ main.php
@@ -1,8 +1,11 @@
 <?php
-if(isset($_REQUEST['submit'])){
-    if($_REQUEST['password']=='admin'&&$_REQUEST['username']=='admin'){
+
+$hashed = password_hash('admin', PASSWORD_DEFAULT);
+if (isset($_REQUEST['submit'])) {
+    if (password_verify($_REQUEST['password'], $hashed)
+        && $_REQUEST['username'] == 'admin') {
         session_start();
-        $_SESSION['user']='admin';
+        $_SESSION['user'] = 'admin';
     } else {
         header('location: login.php');
     }
@@ -16,12 +19,13 @@ if(isset($_REQUEST['submit'])){
     <html lang="de">
     <head>
         <title>Main</title>
-        <link rel="stylesheet" type="text/css" href="form.css" />
+        <link rel="stylesheet" type="text/css" href="form.css"/>
         <script src="js-library.js"></script>
     </head>
     <body>
     <a href="files.php" class="link">Dateien</a>
     <a href="article.php" class="link">Artikel</a>
+    <a href="newuser.php" class="link logout">Neuer Nutzer</a>
     <a href="login.php?logout=true" class="link logout">Logout</a>
 
     </body>
